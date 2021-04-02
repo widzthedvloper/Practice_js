567a25f9228b4feb253a68fbd917a12d6060c70c
Widzmarc Jean Nesly Phelle <widzmarc@gmail.com>
2021-04-01 20:24:30 -0400

    Create modal

--- js/lightbox.js
+++ js/lightbox.js
@@ -2,7 +2,9 @@
 var propLightbox = {
     imgContainer: document.getElementsByClassName('lightbox'),
     image: null,
-    imageSrc: null
+    imageSrc: null,
+    bodyDom: document.getElementsByTagName('body')[0],
+    lightbox_container: null
 };
 
 //object with the lightbox effect methods
@@ -22,7 +24,21 @@ var metLightbox = {
 
     lightbox: function(image){
         propLightbox.imageSrc = window.getComputedStyle(image, null).backgroundImage.slice(5, -2);
-        console.log(propLightbox.imageSrc);
+        propLightbox.bodyDom.appendChild(document.createElement('DIV')).setAttribute('id', 'lightbox-container');
+        propLightbox.lightbox_container = document.getElementById('lightbox-container');
+        propLightbox.lightbox_container.style.width = '100%';
+        propLightbox.lightbox_container.style.height = '100%';
+        propLightbox.lightbox_container.style.position = 'fixed';
+        propLightbox.lightbox_container.style.zIndex = '1000';
+        propLightbox.lightbox_container.style.background = 'rbga(0,0,0,0.8)';
+        propLightbox.lightbox_container.style.top = '0';
+        propLightbox.lightbox_container.style.left = '0';
+
+        propLightbox.lightbox_container.appendChild(document.createElement('DIV')).setAttribute('id', 'modal');
+        propLightbox.modal = document.getElementById('modal');
+
+        propLightbox.modal.style.height = '100%';
+        propLightbox.modal.appendChild(document.createElement('IMG')).setAttribute('src', propLightbox.imageSrc);
     }
 };
 
