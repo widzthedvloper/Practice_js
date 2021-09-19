5e95d0b71b11dccd1c4d37fdf4b2629416d6267b
Widzmarc Jean Nesly Phelle <widzmarc@gmail.com>
2021-09-19 18:22:25 -0400

    Implement slider functionality

---
+++ js/slider.js
@@ -0,0 +1,28 @@
+(function(){
+
+    const propSlider = {
+        slider: document.getElementById("slider"),
+        primerSlide: null
+    }
+
+    const metSlider = {
+        inicio: function() {
+            setInterval(metSlider.moverSlide, 3000);
+        },
+
+        moverSlide: function(){
+            propSlider.slider.style.transition = "all 1s ease";
+            propSlider.slider.style.marginLeft = "-100%";
+
+            setTimeout(function() {
+                propSlider.primerSlide = propSlider.slider.firstElementChild;
+                propSlider.slider.appendChild(propSlider.primerSlide);
+
+                propSlider.slider.style.transition = "unset";
+                propSlider.slider.style.marginLeft = 0;
+            }, 1000)
+        }
+    }
+
+    metSlider.inicio();
+}())
