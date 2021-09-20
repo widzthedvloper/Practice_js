4c046da1122d29b77359a09c04a91e9d15b76717
Widzmarc Jean Nesly Phelle <widzmarc@gmail.com>
2021-09-20 07:42:58 -0400

    Complete validation for the form

--- js/form.js
+++ js/form.js
@@ -3,6 +3,8 @@
     const propForm = {
         form: document.contactForm,
         formElements: document.contactForm.elements,
+        error: null,
+        textError: null,
     }
 
     const metForm = {
@@ -16,6 +18,7 @@
                 }
                 
             }
+            propForm.form.addEventListener('submit', metForm.validateInputs);
         },
 
         focusFunc: function(){
@@ -27,6 +30,30 @@
 
                 this.parentElement.children[1].className = "label";
             }
+        },
+
+        validateInputs: function(e){
+            for (let index = 0; index < propForm.formElements.length; index++) {
+                
+                if(propForm.formElements[index].value === '') {
+                    e.preventDefault();
+                    if(propForm.formElements[index].parentElement.children.length < 3) {
+
+                        propForm.error = document.createElement('p');
+                        propForm.textError = document.createTextNode(`Please provide your ${propForm.formElements[index].name}`);
+                        propForm.error.appendChild(propForm.textError);
+                        propForm.error.className = 'error';
+    
+                        propForm.formElements[index].parentElement.appendChild(propForm.error);
+                    }
+                } else {
+                    if(propForm.formElements[index].parentElement.children.length > 2) {
+                        propForm.error = propForm.formElements[index].parentElement.getElementsByTagName('p')[0];
+                        propForm.formElements[index].parentElement.removeChild(propForm.error);
+                    }
+                }
+            }
+
         }
     }
 
