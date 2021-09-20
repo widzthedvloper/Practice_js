82049f9803e1f451667cdd913d09aed0cae36f37
Widzmarc Jean Nesly Phelle <widzmarc@gmail.com>
2021-09-20 08:00:26 -0400

    Create scroll properties

---
+++ js/scroll.js
@@ -0,0 +1,9 @@
+(function(){
+
+    const propScroll = {
+        position: window.pageYOffset,
+        smoothScroll: document.getElementsByClassName('smooth-scroll'),
+        home: document.getElementsByClassName('home'),
+    }
+
+}())
